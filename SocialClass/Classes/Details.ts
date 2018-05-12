de99aa02ec11b6feb8e4d8b40ff891a284001718
Ankit Arora <arora.ankit7@gmail.com>
2018-05-12 18:44:03 +0530

    Added classes for social

---
+++ SocialClass/Classes/Details.ts
@@ -0,0 +1,11 @@
+class Details {
+    about?: string;
+    otherNames?: string;
+    favouriteQuotes?: string;
+
+    constructor(about: string, otherNames: string, favouriteQuotes: string) {
+        this.about = about;
+        this.otherNames = otherNames;
+        this.favouriteQuotes = favouriteQuotes;
+    }
+}
---
+++ SocialClass/social.ts
@@ -0,0 +1,69 @@
+///<reference path="Classes/Details.ts" />
+
+class About {
+    id: number; //id of the profile
+    name: string;
+    phone?: number[]; //can be more than one phone number
+    address?: string;
+    locations?: string[];
+    currentLocation?: string; 
+    school?: string;
+    college?: string;
+    email: string;
+    familyMember?: string[];
+    otherDetails?: Details;
+
+    constructor(id: number, name: string, phone: number[], address: string, locations: string[], currentLocation: string, school: string, college: string, email: string, familyMembers: string[], otherDetails: Details) {
+        this.id = id;
+        this.name = name;
+        if(phone) { this.phone = phone; }
+        if(address) { this.address = address; }
+        if(locations) { this.locations = locations; }
+        if(currentLocation) { this.currentLocation = currentLocation; }
+        if(school) { this.school = school; }
+        if(college) { this.college = college; }
+        this.email = email;
+        if(this.familyMember) { this.familyMember = this.familyMember; }
+        if(otherDetails) { this.otherDetails = otherDetails; }
+
+    }
+
+    public editName(newName: string) {
+        if(newName != null && newName != "") {
+            this.name = newName;
+        }
+    }
+
+    public editAddress(newAddress: string) {
+        if(newAddress != null && newAddress != "") {
+            this.address = this.address;
+        }
+    }
+
+    public getName():string {
+        return this.name;
+    }
+
+    public getPhoneNumbers(): number[] {
+        if(this.phone) {
+            return this.phone;
+        } 
+    }
+
+    public getAddress(): string {
+        if(this.address) {
+            return address;
+        }
+    }
+
+    public getLocations(): string[] {
+        if(this.locations) {
+            return this.locations;
+        }
+    }
+
+    public getEmail(): string {
+        return this.email;
+    }
+
+}

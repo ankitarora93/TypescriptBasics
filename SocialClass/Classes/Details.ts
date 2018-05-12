dbcc27d474024c1baa28369b1c6ff17e1182b22c
Ankit Arora <arora.ankit7@gmail.com>
2018-05-12 18:50:01 +0530

    Added setters and getters

--- SocialClass/Classes/Details.ts
+++ SocialClass/Classes/Details.ts
@@ -8,4 +8,38 @@ class Details {
         this.otherNames = otherNames;
         this.favouriteQuotes = favouriteQuotes;
     }
+
+    public setAbout(newAbout: string) {
+        if(newAbout != null && newAbout != "") {
+            this.about = newAbout;
+        }
+    }
+
+    public setOtherNames(newOtherName: string) {
+        if(newOtherName != null || newOtherName != "") {
+            this.otherNames = newOtherName;
+        }
+    }
+
+    public setFavouriteQuote(quote: string) {
+        if(quote != null || quote != "") {
+            this.favouriteQuotes = quote;
+        }
+    }
+
+    public getAbout(): string {
+        if(this.about && this.about != "") {
+            return this.about; 
+        } else {
+            //throwSomeError();
+        }
+    }
+
+    public getOtherName(): string {
+        if(this.otherNames && this.otherNames != "") {
+            return this.otherNames;
+        } else {
+            //throwSomeError();
+        }
+    }
 }
